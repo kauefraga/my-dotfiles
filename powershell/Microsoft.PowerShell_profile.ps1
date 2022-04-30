07ea689fe46b66d0c2d6fe4ff673880caec0313f
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-04-30 12:17:31 -0300

    :sparkles: feat: using starship instead of ohmyposh

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,13 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-
-# Install oh-my-posh with chocolatey
-
 # Configuration
-$omp_config = Join-Path $PSScriptRoot ".\kauef.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+Invoke-Expression (&starship init powershell)
 
 # Imports
-# Import-Module -Name z
 Import-Module -Name Terminal-Icons
 
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
