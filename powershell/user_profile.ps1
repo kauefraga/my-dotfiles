cf8963b881821c619bba537b7abd1300fc3140f5
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-05-29 12:59:47 -0300

    :recycle: chore: the best conf

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,39 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Configuration
+Invoke-Expression (&starship init powershell)
+
+# Imports
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Program Files\OpenSSH-Win64\ssh.exe"
+$env:NVIM_HOME = "C:\Users\kauefraga\AppData\Local\nvim"
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Work dir
+cd ~
+clear
