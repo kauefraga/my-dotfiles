f24fc8a2d09b8218d8e50d05270a241e8deddb5e
kauefraga <kauefragarodrigues456@gmail.com>
2022-08-03 21:28:34 -0300

    :recycle: feat: add install packages

--- ez-setup/win.ps1
+++ ez-setup/win.ps1
@@ -8,63 +8,39 @@
 #  |__/  \__/|__/      github.com/kauefraga/my-dotfiles
 Clear-Host
 
-If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+Write-Host "[ script ] Starting..."
+
+if (-Not (Test-Path "$env:userprofile/scoop"))
 {
-  Write-Warning "You don't have Administrator rights"
-  exit
+  Invoke-RestMethod "get.scoop.sh" | Invoke-Expression
+  # irm get.scoop.sh | iex -> same as above
 }
 
-Write-Host "[script] Starting..."
+<# TODO
+  ask the user about which packages he wants to install
+  parse the list of packages and install them
+#>
 
-# Install Chocolatey package manager
-#Set-ExecutionPolicy AllSigned
-#Set-ExecutionPolicy Bypass -Scope Process -Force
-#iwr -useb "https://community.chocolatey.org/install.ps1" | Invoke-Expression
+# Install packages :: initializing
+$option = Read-Host "[ script ] Do you want to install apps? [y/n] "
 
-Write-Host "[script] Installing..."
-# Install general stuff
-[void]choco install -y winrar git openssh mingw ccls 
+if ($option -eq "y")
+{
+  $packages = Get-Content "./packages.json" | ConvertFrom-Json
 
-# Install lang and runtimes
-[void]choco install -y python go nvm
-
-# Install utils and prettiers
-[void]choco install -y neovim alacritty starship
-[void]Install-Module -Name Terminal-Icons -Repository PSGallery
-
-# Install main apps
-[void]choco install -y vivaldi discord
-Write-Host "[script] Downloads finished"
-
-
-Write-Host "[script] Configuring..."
-cd $env:USERPROFILE
-
-git clone https://github.com/kauefraga/my-dotfiles.git dotfiles
-
-# Powershell config
-mkdir .\.config\powershell
-iwr -Outfile .\powershell\user_profile.ps1 -useb "https://raw.githubusercontent.com/kauefraga/my-dotfiles/main/powershell/user_profile.ps1"
-
-# Powershell init
-mkdir .\Documents\WindowsPowerShell
-". $env:USERPROFILE\.config\powershell\user_profile.ps1" > .\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-
-# Nvim config
-mkdir $env:LOCALAPPDATA\nvim
-mv .\dotfiles\.config\nvim\* $env:LOCALAPPDATA\nvim
-
-# Install vim-plug
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-  ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-
-nvim +PlugInstall
+  Write-Host "[ script ] Installing packages..."
+  scoop install $packages.basics
+  scoop install $packages.compilers
+  scoop install $packages.terminal
+  scoop install $packages.langs
+  scoop install $packages.frufru
+  scoop install $packages.extras
+}
+# Install packages :: finishing
 
 # ...
 # Config alacritty
 # Run spotify script
 
-
-
-
-
+# Invoke-WebRequest -UseBasicParsing "raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1" | Invoke-Expression
+iwr -useb "raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1" | iex
