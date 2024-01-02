99c48130352d2117d5abfebcdde23a110d1cec09
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-02 17:18:46 -0300

    :recycle: chore: update scripts

--- windows/powershell/user_profile.ps1
+++ windows/powershell/user_profile.ps1
@@ -1,44 +1,14 @@
-# set PowerShell to UTF-8
+# Set powershell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-# Configuration
+# Initialize fast node manager
+fnm env --use-on-cd | Out-String | Invoke-Expression
+
+# Initialize starship
 Invoke-Expression (&starship init powershell)
 
-# PSReadLine
-#Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-# Autocompleteion for Arrow keys
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-Set-PSReadLineOption -ShowToolTips
-
-# Fzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Env
-$env:GIT_SSH = "C:\Program Files\OpenSSH-Win64\ssh.exe"
-$env:NVIM_HOME = "$env:USERPROFILE\AppData\Local\nvim"
-
-# Alias
+# Aliases
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
-# ~
-clear
