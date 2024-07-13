d0be51f9903497ebe97fb5a170088d7fde01aac9
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-07-13 12:37:41 -0300

    config: update fish config

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,42 +1,42 @@
-# bun
+# -- bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
-# jdk
-set --export JAVA_HOME /usr/lib/jvm/jdk-21-oracle-x64
+# -- jdk
+# set --export JAVA_HOME /usr/lib/jvm/jdk-21-oracle-x64
 
-# gatling
-set --export GATLING_HOME $HOME/gatling
+# -- gatling
+# set --export GATLING_HOME $HOME/gatling
 
-# ruby
-set --export GEM_HOME "$HOME/.gems"
-set --export PATH $HOME/.gems/bin $PATH
+# -- ruby
+# set --export GEM_HOME "$HOME/.gems"
+# set --export PATH $HOME/.gems/bin $PATH
 
-# PATH
+# -- $path
 set --export PATH /opt/nvim-linux64/bin $PATH # neovim
 set --export PATH /usr/local/go/bin $PATH # go
 set --export PATH $HOME/.cargo/bin $PATH # rust
 set --export PATH $HOME/.bun/bin $PATH # bun
-set --export PATH $JAVA_HOME $PATH # java
+# set --export PATH $JAVA_HOME $PATH # java
 
 set --export PATH $HOME/go/bin $PATH # go binaries
 set --export PATH $HOME/.local/bin $PATH
-set --export PATH $HOME/www/anime-archive/bin/linux $PATH # anime archive
+# set --export PATH $HOME/www/anime-archive/bin/linux $PATH # anime archive
 
-# Configs
+# -- configs
 set --export GPG_TTY $(tty)
 
 starship init fish | source
 
 zoxide init fish | source
 
-# Aliases
-alias bat='batcat'
+# -- aliases
 alias vim='nvim'
 alias python='python3'
 alias pip='pip3'
 alias ca='cargo'
 
+# -- git aliases
 alias g='git'
 alias ga='git add'
 alias gc='git commit'
@@ -45,8 +45,9 @@ alias gs='git status'
 alias gssb='git status -sb'
 alias gl='git log --oneline'
 alias gb='git branch'
+alias gch='git checkout'
 
-# Functions
+# -- functions
 function gitsetup
     git config --global user.email "rkauefraga@gmail.com"
     git config --global user.name "Kauê Fraga Rodrigues"
@@ -62,7 +63,3 @@ function sshkrl
     ssh-add ~/.ssh/id_ed25519
 end
 
-if set -q ZELLIJ
-else
-  zellij
-end
