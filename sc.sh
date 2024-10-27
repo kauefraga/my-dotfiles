46bfdb78f8caf46f9d7a34495c3eb44cebd73890
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-10-27 16:26:56 -0300

    feat: create script to save configs

---
+++ sc.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+# -- [s]ave [c]onfigs
+
+cp ~/.wezterm.lua linux/
+
+cp ~/.gitconfig linux/
+cp ~/.githelpers linux/
+
+cp ~/.config/fish/config.fish linux/.config/fish/
+
+cp ~/.config/starship.toml linux/.config/
+
+cp ~/.config/alacritty/* linux/.config/alacritty
